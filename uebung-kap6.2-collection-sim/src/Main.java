865132afadb99c94f0802a134f317df06c1afba0
alexpietsch <54153428+alexpietsch@users.noreply.github.com>
2025-06-04 12:21:52 +0200

    add files

---
+++ uebung-kap6.2-collection-sim/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayListTest.main(args);
+
+        LinkedListTest.main(args);
+
+        HashMapTest.main(args);
+
+        TreeMapTest.main(args);
+
+        HashSetTest.main(args);
+
+        TreeSetTest.main(args);
+
+    }
+
+}
